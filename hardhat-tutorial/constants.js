1605888a38e6ebf5e950f72412ec41a92120fbf6
Geelbuik3421 <60668064+Geelbuik3421@users.noreply.github.com>
2022-12-30 20:27:24 +0100

    Geen

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x13433adA70E555045a2870c50231fd26169CbC92";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
